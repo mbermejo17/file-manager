516c9b065aa9425b013bde7e2240fe647e2cbd76
Manuel Bermejo Lopez <mbermejo17@gmail.com>
2018-06-23 21:34:36 +0200

    reajustes codigo

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ app.set("views", path.join(__dirname, "app", "views"));
 
 // Header settings
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("X-Powered-By", "Custom Engine");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("X-Powered-By", "Custom Engine");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // Routes handle requests
@@ -45,19 +45,19 @@ app.use("/files", fileRoutes);
 
 // Error handle requests
 app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    next(error);
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    res.status(error.status || 500).send();
-    /* res.json({
-        error: {
-            message: error.message
-        }
-    }); */
+  console.log(error);
+  res.status(error.status || 500).send();
+  /* res.json({
+      error: {
+          message: error.message
+      }
+  }); */
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -10,77 +10,77 @@ global.appRoot = path.resolve(__dirname);
 
 //const server = http.createServer(app);
 let httpsServer = https.createServer({
-    key: fs.readFileSync(global.appRoot + '/app/certs/ssl.key'),
-    cert: fs.readFileSync(global.appRoot + '/app/certs/ssl.crt')
-}, app).listen(port, function() {
-    console.log("https server listening on port " + port + "...");
+  key: fs.readFileSync(global.appRoot + '/app/certs/ssl.key'),
+  cert: fs.readFileSync(global.appRoot + '/app/certs/ssl.crt')
+}, app).listen(port, function () {
+  console.log("https server listening on port " + port + "...");
 });
 
 
-let noop = () => {};
+let noop = () => { };
 
 let wss = new WebSocketServer({
-    //server: httpsServer,
-    path: '/client',
-    noServer: true
+  //server: httpsServer,
+  path: '/client',
+  noServer: true
 });
 
 let wssRemoteRoom = new WebSocketServer({
-    //server: httpsServer,
-    path: '/room',
-    noServer: true
+  //server: httpsServer,
+  path: '/room',
+  noServer: true
 });
 
 
 httpsServer.on('uncaughtException', (request, response, route, error) => {
-    console.error(error.stack);
-    response.send(error);
+  console.error(error.stack);
+  response.send(error);
 });
 httpsServer.on('upgrade', (req, socket, head) => {
-    const pathname = url.parse(req.url).pathname;
+  const pathname = url.parse(req.url).pathname;
 
-    if (pathname === '/client') {
-        wss.handleUpgrade(req, socket, head, (ws) => {
-            wss.emit('connection', ws, req);
-        });
-    } else if (pathname === '/room') {
-        wssRemoteRoom.handleUpgrade(req, socket, head, (ws) => {
-            wssRemoteRoom.emit('connection', ws, req);
-        });
-    } else {
-        socket.destroy();
-    }
+  if (pathname === '/client') {
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit('connection', ws, req);
+    });
+  } else if (pathname === '/room') {
+    wssRemoteRoom.handleUpgrade(req, socket, head, (ws) => {
+      wssRemoteRoom.emit('connection', ws, req);
+    });
+  } else {
+    socket.destroy();
+  }
 });
 
 
 
-wss.getUniqueID = function() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
-    return s4() + s4() + '-' + s4();
+wss.getUniqueID = function () {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4();
 };
 const interval = setInterval(function sendKeepAlive() {
-    wss.clients.forEach(function each(ws) {
-        console.log('Client.ID ' + ws.id + ' isAlive ' + ws.isAlive);
-        if (ws.isAlive === false) return ws.terminate();
-        ws.isAlive = false;
-        ws.send(JSON.stringify({
-            'event': 'KeepAlive',
-            'message': ''
-        }));
-        ws.ping(noop);
-    });
-    wssRemoteRoom.clients.forEach(function each(wsRemoteRoom) {
-        console.log('Room.ID ' + wsRemoteRoom.id + ' isAlive ' + wsRemoteRoom.isAlive);
-        if (wsRemoteRoom.isAlive === false) return wsRemoteRoom.terminate();
-        wsRemoteRoom.isAlive = false;
-        wsRemoteRoom.send(JSON.stringify({
-            'event': 'KeepAlive',
-            'message': ''
-        }));
-        wsRemoteRoom.ping(noop);
-    });
+  wss.clients.forEach(function each(ws) {
+    console.log('Client.ID ' + ws.id + ' isAlive ' + ws.isAlive);
+    if (ws.isAlive === false) return ws.terminate();
+    ws.isAlive = false;
+    ws.send(JSON.stringify({
+      'event': 'KeepAlive',
+      'message': ''
+    }));
+    ws.ping(noop);
+  });
+  wssRemoteRoom.clients.forEach(function each(wsRemoteRoom) {
+    console.log('Room.ID ' + wsRemoteRoom.id + ' isAlive ' + wsRemoteRoom.isAlive);
+    if (wsRemoteRoom.isAlive === false) return wsRemoteRoom.terminate();
+    wsRemoteRoom.isAlive = false;
+    wsRemoteRoom.send(JSON.stringify({
+      'event': 'KeepAlive',
+      'message': ''
+    }));
+    wsRemoteRoom.ping(noop);
+  });
 }, 30000);
 
 
@@ -90,69 +90,69 @@ const interval = setInterval(function sendKeepAlive() {
 wss.aSockets = [];
 
 wss.on('connection', function connection(ws, req) {
+  ws.isAlive = true;
+  if (!wss.aSockets[userName]) {
+    wss.aSockets[userName] = [];
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    //console.log('HandleID ' + ws._socket._handle.fd);
+    ws.id = wss.getUniqueID();
+    //wss.clients.forEach(function each(client) {
+    console.log('Client.ID: ' + ws.id + ' Connected');
+    //});
+  }
+  wss.aSockets[userName].push(socketID);
+  console.log(wss.aSockets);
+  //console.dir(req.headers['sec-websocket-key']);
+  ws.send(JSON.stringify({
+    'event': 'Connected',
+    'message': `Client ID -> ${ws.id}  Connected!!!. Your IP  ${ip}`
+  }));
+  ws.on('message', (data) => {
+    let jsonData = JSON.parse(data);
+    let eventType = jsonData.event;
+    let message = jsonData.message;
+    if (eventType === 'KeepAlive')
+      ws.isAlive = true;
+    //ws.send(`Data received from IP ${ip} : ${data.toString()} `) // echo-server
+  })
+  ws.on('pong', () => {
     ws.isAlive = true;
-    if (!wss.aSockets[userName]) {
-        wss.aSockets[userName] = [];
-        let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        //console.log('HandleID ' + ws._socket._handle.fd);
-        ws.id = wss.getUniqueID();
-        //wss.clients.forEach(function each(client) {
-        console.log('Client.ID: ' + ws.id + ' Connected');
-        //});
-    }
-    wss.aSockets[userName].push(socketID);
-    console.log(wss.aSockets);
-    //console.dir(req.headers['sec-websocket-key']);
-    ws.send(JSON.stringify({
-        'event': 'Connected',
-        'message': `Client ID -> ${ws.id}  Connected!!!. Your IP  ${ip}`
-    }));
-    ws.on('message', (data) => {
-        let jsonData = JSON.parse(data);
-        let eventType = jsonData.event;
-        let message = jsonData.message;
-        if (eventType === 'KeepAlive')
-            ws.isAlive = true;
-        //ws.send(`Data received from IP ${ip} : ${data.toString()} `) // echo-server
-    })
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    });
-    ws.on("close", function() {
-        console.log("Client.ID: " + ws.id + ' Disconnected');
-    });
+  });
+  ws.on("close", function () {
+    console.log("Client.ID: " + ws.id + ' Disconnected');
+  });
 });
 
 ///////////////////////////////////
 //Eventos WebSockets Salas remotas
 //////////////////////////////////
 wssRemoteRoom.on('connection', function connection(wsRemoteRoom, req) {
-    wsRemoteRoom.isAlive = true;
-    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('HandleID ' + wsRemoteRoom._socket._handle.fd);
-    wsRemoteRoom.id = wss.getUniqueID();
-    wssRemoteRoom.clients.forEach(function each(client) {
-        console.log('Room.ID: ' + client.id + ' Connected');
-    });
+  wsRemoteRoom.isAlive = true;
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('HandleID ' + wsRemoteRoom._socket._handle.fd);
+  wsRemoteRoom.id = wss.getUniqueID();
+  wssRemoteRoom.clients.forEach(function each(client) {
+    console.log('Room.ID: ' + client.id + ' Connected');
+  });
 
-    //console.dir(req.headers['sec-websocket-key']);
-    wsRemoteRoom.send(JSON.stringify({
-        'event': 'Connected',
-        'message': `Room ID -> ${wsRemoteRoom.id}  Connected!!!. Your IP  ${ip}`
-    }));
-    wsRemoteRoom.on('message', (data) => {
-        let jsonData = JSON.parse(data);
-        let eventType = jsonData.event;
-        let message = jsonData.message;
-        if (eventType === 'KeepAlive')
-            wsRemoteRoom.isAlive = true;
-        //ws.send(`Data received from IP ${ip} : ${data.toString()} `) // echo-server
-    })
-    wsRemoteRoom.on('pong', () => {
-        wsRemoteRoom.isAlive = true;
-    });
-    wsRemoteRoom.on("close", function() {
-        console.log("Room.ID: " + wsRemoteRoom.id + ' disconnected');
-    });
+  //console.dir(req.headers['sec-websocket-key']);
+  wsRemoteRoom.send(JSON.stringify({
+    'event': 'Connected',
+    'message': `Room ID -> ${wsRemoteRoom.id}  Connected!!!. Your IP  ${ip}`
+  }));
+  wsRemoteRoom.on('message', (data) => {
+    let jsonData = JSON.parse(data);
+    let eventType = jsonData.event;
+    let message = jsonData.message;
+    if (eventType === 'KeepAlive')
+      wsRemoteRoom.isAlive = true;
+    //ws.send(`Data received from IP ${ip} : ${data.toString()} `) // echo-server
+  })
+  wsRemoteRoom.on('pong', () => {
+    wsRemoteRoom.isAlive = true;
+  });
+  wsRemoteRoom.on("close", function () {
+    console.log("Room.ID: " + wsRemoteRoom.id + ' disconnected');
+  });
 });
 //server.listen(port);
